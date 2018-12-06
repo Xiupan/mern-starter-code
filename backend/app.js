4d5c01fa5349a3f9b924abdd8bec96d5b85d51ce
Alan Hong <xiupan@gmail.com>
2018-12-06 16:31:49 -0600

    Basic Step 2 Complete.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const server = http.createServer(app)
 const port = 5000
+
+console.log('Connecting to Local Database.');
+mongoose.connect('mongodb://localhost/your-local-db-name', { useNewUrlParser: true })
+
 server.listen(port)
 console.log(`NodeJS Server running on port ${port}.`);
